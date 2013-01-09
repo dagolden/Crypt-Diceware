a1d3cf50f347637a724a34e7ab358cf5472eb46a
David Golden <dagolden@cpan.org>
2013-01-08 21:20:21 -0500

    initial import

---
+++ lib/Crypt/Diceware.pm
@@ -0,0 +1,106 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Crypt::Diceware;
+# ABSTRACT: Random passphrase generator loosely based on the Diceware algorithm
+# VERSION
+
+use Class::Load qw/load_class/;
+use Data::Entropy::Algorithms qw/pick_r/;
+
+use Sub::Exporter -setup => {
+  exports => [ words   => \'_build_words' ],
+  groups  => { default => [qw/words/] },
+};
+
+sub _build_words {
+  my ( $class, $name, $arg ) = @_;
+  $arg ||= {};
+  my $word_class = $arg->{wordlist} || 'Common';
+  unless ( $word_class =~ /::/ ) {
+    $word_class = "Crypt::Diceware::Wordlist::$word_class";
+  }
+  load_class($word_class);
+  my $list = do {
+    no strict 'refs';
+    \@{"${word_class}::Words"};
+  };
+  return sub {
+    my ($n) = @_;
+    return unless $n > 0;
+    return map { pick_r($list) } 1 .. int($n);
+  };
+}
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use Crypt::Diceware;
+  my @phrase = words(4);
+
+  # with alternate word lists
+  use Crypt::Diceware words => { wordlist => 'Original' };
+  use Crypt::Diceware words => { wordlist => 'Beale' };
+
+=head1 DESCRIPTION
+
+This module generates a random passphrase of words based loosely on the
+L<Diceware|http://world.std.com/~reinhold/diceware.html> algorithm by Arnold G.
+Reinhold.
+
+A passphrase consists of a randomly selected words chosen from a list of over
+seven thousand words.  A passphrase of four or five words is likely to be
+stronger than a typical human-generated password, which tends to be too-short
+and over-sample common letters ("e") and numbers ("1").
+
+Words are selected by random numbers from L<Data::Entropy>, which is reasonably
+cryptographically strong.
+
+=head1 USAGE
+
+The module exports a single subroutine, C<words>, by default, which uses the
+L<Crypt::Diceware::Wordlist::Common> word list.  An alternate wordlist may be
+specified:
+
+  use Crypt::Diceware words => { wordlist => 'Original' };
+
+Exporting is done via L<Sub::Exporter> so any of its features may be used:
+
+  use Crypt::Diceware words => { -as => 'passphrase' };
+  my @phrase = passphrase(4);
+
+=head2 words
+
+  my @phrase = words(4);
+
+Takes a positive integer argument and returns a list of that many
+randomly-selected words (rounding down to an integer length).  Returns the
+empty list if the argument is missing or not a positive number.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Diceware|http://world.std.com/~reinhold/diceware.html>
+* L<Crypt::Diceware::Wordlist::Common>
+* L<Crypt::Diceware::Wordlist::Original>
+* L<Crypt::Diceware::Wordlist::Beale>
+
+Other CPAN passphrase generators:
+
+=for :list
+* L<Crypt::PW44>
+* L<Crypt::XkcdPassword>
+
+About password strength in general:
+
+=for :list
+* L<Password Strength (XKCD)|http://xkcd.com/936/>
+* L<Password Strength (Wikipedia)|http://en.wikipedia.org/wiki/Password_strength>
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
---
+++ t/basic.t
@@ -0,0 +1,47 @@
+use 5.010;
+use strict;
+use warnings;
+use Test::More 0.96;
+
+use Crypt::Diceware;
+use Crypt::Diceware words => { -as => 'words_common',   wordlist => 'Common' };
+use Crypt::Diceware words => { -as => 'words_original', wordlist => 'Original' };
+use Crypt::Diceware words => { -as => 'words_beale',    wordlist => 'Beale' };
+
+my $count;
+
+is( $count = () = words(),   0, "words() without args returns empty list" );
+is( $count = () = words(-1), 0, "words() with negative arg returns empty list" );
+is( $count = () = words("abc"),
+  0, "words() with non-numeric arg returns empty list" );
+
+for my $n ( 1 .. 4 ) {
+  is( $count = () = words($n), $n, "words($n) returns list of $n words" );
+}
+
+is( $count = () = words(3.14), 3, "words(3.14) returns list of 3 words" );
+
+isnt(
+  join( " ", words(5) ),
+  join( " ", words(5) ),
+  "words(N) != words(N) (default)"
+);
+
+isnt(
+  join( " ", words_common(5) ),
+  join( " ", words_common(5) ),
+  "words_common(N) != words_common(N))"
+);
+isnt(
+  join( " ", words_original(5) ),
+  join( " ", words_original(5) ),
+  "words_original(N) != words_original(N)"
+);
+isnt(
+  join( " ", words_beale(5) ),
+  join( " ", words_beale(5) ),
+  "words_beale(N) != words_beale(N)"
+);
+
+done_testing;
+# COPYRIGHT
